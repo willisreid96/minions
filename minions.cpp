012ebb20cc9efa42317476b985e4356e9a76f91b
willisreid96 <77948816+willisreid96@users.noreply.github.com>
2021-10-04 20:09:07 -0700

    Add files via upload

---
+++ minions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main (int argc, char** argv) {
+  if (argc < 2) {
+    std::cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
+    return -1;
+  }
+  //Your code goes here
+  
+  return 0;
+}

0f39a629188334baf5ae70946505ac6403675062
willisreid96 <77948816+willisreid96@users.noreply.github.com>
2021-10-05 11:05:14 -0700

    Update minions.cpp

--- minions.cpp
+++ minions.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
+#include <vector>	
 
 
-int main (int argc, char** argv) {
-  if (argc < 2) {
-    std::cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
-    return -1;
-  }
-  //Your code goes here
-  
-  return 0;
+void min(int n) {
+    std::cout << "Hello! I am minion " << n << std::endl;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "usage: " << argv[0] << " <nbminions>\n";
+        return -1;
+    }
+    //code goes here
+    int num = atoi(argv[1]);
+
+    std::thread gru[num];
+
+    for (int n = 0; n < num; ++n) {
+        gru[n] = std::thread(min, n);
+    }
+
+    for (int n = 0; n < num; n++) {
+        gru[n].join();
+    }
+    std::cout << "hello minions! I am the Overlord!" << std::endl;
+    return 0;
 }
